eb973216a8c2f6e3b563230e59860a3ce151d22b
Franklin Fanya <franklinfanya@gmail.com>
2025-12-30 06:03:24 +0100

    update

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://localhost:5174', 
-    'https://velto-steel.vercel.app',
+    'https://velto-theta.vercel.app/',
     'https://velto-management.vercel.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
